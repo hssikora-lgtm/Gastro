f1bfb349e9bfdc5061e183506877045c503e6a4b
hssikora-lgtm <hs.sikora@gmail.com>
2026-02-26 21:57:55 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
